cd04f3f2d8bca13d4980d9d0ed6d3f09102e09f0
fxcrypto297-lang <fxcrypto297@gmail.com>
2025-09-01 00:22:34 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <div className="p-4">DOM Terminal MVP</div>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
